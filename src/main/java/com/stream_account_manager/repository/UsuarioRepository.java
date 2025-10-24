602d04f53812e3c4e42daf52849fa207d14a8db2
unknown <franco.hopper.riwi@gmail.com>
2025-10-24 13:53:50 -0500

    Preparación para refactor. Se eliminaron todos los modelos, servicios, repositorios, etc.

--- src/main/java/com/stream_account_manager/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stream_account_manager.repository;
-
-import com.stream_account_manager.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
