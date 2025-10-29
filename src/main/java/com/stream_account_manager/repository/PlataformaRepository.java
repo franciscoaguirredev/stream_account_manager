74b7d6e5fd55bd3dc7b49bf7ef73c5597e2d5281
Samuel <samueldiaz180405@gmail.com>
2025-10-28 21:42:32 -0500

    config: actualizar application.properties con configuración de Swagger y cambiar ddl-auto a update

--- src/main/java/com/stream_account_manager/repository/PlataformaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stream_account_manager.repository;
-
-import com.stream_account_manager.model.Plataforma;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface PlataformaRepository extends JpaRepository<Plataforma,Long>{
-}
