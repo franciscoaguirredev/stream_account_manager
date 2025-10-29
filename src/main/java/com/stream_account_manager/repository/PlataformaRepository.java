fdcee44ddeb529890ae4e123d7d687506f2851fe
Samuel <samueldiaz180405@gmail.com>
2025-10-28 21:48:35 -0500

    chore: agregar repositories existentes del proyecto

---
+++ src/main/java/com/stream_account_manager/repository/PlataformaRepository.java
@@ -0,0 +1,7 @@
+package com.stream_account_manager.repository;
+
+import com.stream_account_manager.model.Plataforma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlataformaRepository extends JpaRepository<Plataforma, Long> {
+}
